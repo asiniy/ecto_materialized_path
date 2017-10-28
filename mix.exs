45fa649897cf008c38af1a5a38b4549104b07010
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-10-28 12:45:53 +0300

    0.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EctoMaterializedPath.Mixfile do
   use Mix.Project
 
   @project_url "https://github.com/asiniy/ecto_materialized_path"
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
