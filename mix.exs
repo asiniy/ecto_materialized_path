dc81eca10a37f1df39c4b6e5739844e04a3d1923
c4710n <c4710n@users.noreply.github.com>
2021-09-25 13:34:10 +0800

    fix: fix compile warning
    `extra_applications` is supported from Elixir 1.5. Because of that,
    I adjusted the miminal requirement of Elixir.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule EctoMaterializedPath.Mixfile do
     [
       app: :ecto_materialized_path,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -20,27 +20,20 @@ defmodule EctoMaterializedPath.Mixfile do
     ]
   end
 
-
   defp elixirc_paths(:test), do: elixirc_paths() ++ ["test/support", "test/dummy"]
   defp elixirc_paths(_),     do: elixirc_paths()
   defp elixirc_paths,        do: ["lib"]
 
   def application do
     [
-      applications: app_list(Mix.env),
+      extra_applications: [:logger]
     ]
   end
-
-  def app_list(:test), do: app_list() ++ [:ecto, :ex_machina]
-  def app_list(_), do: app_list()
-  def app_list, do: [:logger]
 
   defp deps do
     [
      {:ecto, ">= 3.0.0"},
-
      {:ex_machina, ">= 0.0.0", only: :test},
-
      {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
